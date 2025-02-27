164af6d601a281768d320be07705a730cf8bb06f
Saswat Pal <saswat2001@gmail.com>
2025-02-27 10:36:52 +0530

    Produce event and serialize

--- AuthService/app/src/main/java/authservice/eventProducer/UserInfoProducer.java
+++ AuthService/app/src/main/java/authservice/eventProducer/UserInfoProducer.java
@@ -1,7 +1,32 @@
 package authservice.eventProducer;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import authservice.model.UserInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserInfoProducer {
+    private final KafkaTemplate<String, UserInfoDto> kafkaTemplate;
+
+    @Value("${spring.kafka.topic.name}")
+    private String TOPIC_NAME;
+
+    @Autowired
+    UserInfoProducer(KafkaTemplate<String,UserInfoDto> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEventToKafka(UserInfoDto userInfoDto) {
+        Message<UserInfoDto> message = MessageBuilder.withPayload(userInfoDto)
+                .setHeader(KafkaHeaders.TOPIC,TOPIC_NAME)
+                .build();
+        kafkaTemplate.send(message);
+    }
+
 }
---
+++ AuthService/app/src/main/java/authservice/serializer/UserInfoSerializer.java
@@ -0,0 +1,32 @@
+package authservice.serializer;
+
+import authservice.model.UserInfoDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.io.SerializationException;
+import io.jsonwebtoken.io.Serializer;
+
+import java.io.OutputStream;
+
+public class UserInfoSerializer implements Serializer<UserInfoDto> {
+
+    @Override
+    public byte[] serialize(UserInfoDto userInfoDto) throws SerializationException {
+        byte[] retVal = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            retVal = objectMapper.writeValueAsString(userInfoDto).getBytes();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return retVal;
+
+    }
+
+    @Override
+    public void serialize(UserInfoDto userInfoDto, OutputStream out) throws SerializationException {
+
+    }
+
+
+}
