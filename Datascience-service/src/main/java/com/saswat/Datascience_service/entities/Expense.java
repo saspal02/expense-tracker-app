146f95473265fe945930558b5b4a96806f4dcc53
Saswat Pal <saswat2001@gmail.com>
2025-05-26 20:57:09 +0530

    Update Expense entity and docker-compose config

--- Datascience-service/src/main/java/com/saswat/Datascience_service/entities/Expense.java
+++ Datascience-service/src/main/java/com/saswat/Datascience_service/entities/Expense.java
@@ -1,5 +1,9 @@
 package com.saswat.Datascience_service.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +11,9 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Expense {
     private String amount;
     private String merchant;
